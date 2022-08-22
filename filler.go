8610c910b9f64250c579c9fd7d241cc1ea8b072f
kranzan <HQ&5N/89!=lkM?>
2022-08-22 12:01:01 +0200

    headache

--- filler.go
+++ filler.go
@@ -18,28 +18,28 @@ var seededRand *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
 //Fill fills an already existing table with the structure of: 'id','text','value' in postgres.
 //It fills it witha random String with the lenght of 15 characters and a random integer with 4 digits.
 //With amonut, you specify the amount of inserts that you want to create.
-func Fill(dbname string, tableName string, amount int) error {
+func Fill(dbname string, tableName string, amount int) {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
 		//psqlInfo := fmt.Sprintf("host=%s port=%d user=%s  dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
+		Host, Port, User, Password, dbname)
 
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
-		return err
+		fmt.Println(err)
 	}
 
 	for i := 0; i < amount; i++ {
 		insertStmt := `insert into $1("text","value") values ($2,$3)`
 		_, err = db.Exec(insertStmt, tableName, String(15), rand.Intn(9999-0))
 		if err != nil {
-			return err
+			fmt.Println(err)
 		}
 	}
 
 	defer func(db *sql.DB) {
 		err := db.Close()
 		if err != nil {
-			return err
+			fmt.Println(err)
 		}
 	}(db)
 }
